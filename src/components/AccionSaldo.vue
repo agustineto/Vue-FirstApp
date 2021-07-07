101f3c0534a40f2a02e3c686c81d25b8807a78b4
agustin <agustinzavalat@gmail.com>
2021-07-06 19:51:44 -0500

    accion botones

--- src/components/AccionSaldo.vue
+++ src/components/AccionSaldo.vue
@@ -1,16 +1,21 @@
 <template>
-  <button @click="accion">{{texto}}</button>
+  <button @click="accion" :disabled="desactivar">{{texto}}</button>
 </template>
 
 <script>
 export default {
     props: {
-        texto: String
+        texto: String,
+        desactivar:{
+            type:Boolean,
+            default: false
+        }
     },
     methods:{
         accion(){
           this.$emit('accion')  
-        }
+        },
+
     }
 
 }
--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -1,12 +1,15 @@
 <template>
     <h3>Tipo de cuenta: {{tipo}}</h3>
-    <h3>Tu Saldo actual: {{saldo}}</h3>
+    <h3 v-if="saldo > 200 ">Tu Saldo actual: {{saldo}}</h3>
+    <h3 v-else style="color:red">Tu Saldo actual: {{saldo}} </h3>
     <h3>Estado {{ estado ? 'Activa': 'Desactivada'}}</h3>
+
     <div v-for="(servicio, index) in servicios" :key="index">
       {{index +1}} - {{ servicio }}
     </div>
-    <AccionSaldo texto="Aumentar Saldo" @accion="aumentar"/>
-    <AccionSaldo texto="Disminuir Saldo" @accion="disminuir"/>
+
+    <AccionSaldo texto="Aumentar Saldo" @accion="aumentar" />
+    <AccionSaldo texto="Disminuir Saldo" @accion="disminuir" :disabled="desactivar"/>
 </template>
 
 <script>
@@ -21,14 +24,20 @@ export default {
             saldo: 1000,
             tipo: 'Debito',
             estado: true,
-            servicios:['abono', 'transferencias', '']
+            servicios:['abono', 'transferencias', ''],
+            desactivar: false
         }
     },
     methods:{
         aumentar(){
             this.saldo = this.saldo + 100
+             this.desactivar= false;
         },
         disminuir(){
+            if(this.saldo === 0){
+                this.desactivar= true;
+                return
+            }
             this.saldo = this.saldo - 100
         }
 
